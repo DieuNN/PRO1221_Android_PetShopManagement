03568d8e8bd7bdd8417a321ac9d698e77bc88fee
dieu <nongngocdieu20122002@gmail.com>
2021-11-01 16:32:36 +0700

    Change LauncherScreen.kt

--- app/src/main/java/com/example/pro1221_android_petshopmanagement/screen/LauncherScreen.kt
+++ app/src/main/java/com/example/pro1221_android_petshopmanagement/screen/LauncherScreen.kt
@@ -1,13 +1,17 @@
 package com.example.pro1221_android_petshopmanagement.screen
 
 import android.annotation.SuppressLint
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,11 +19,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pro1221_android_petshopmanagement.R
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.DelicateCoroutinesApi
+
 
 class LauncherScreen : ComponentActivity() {
     @DelicateCoroutinesApi
@@ -28,18 +36,27 @@ class LauncherScreen : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LauncherScreenMainView()
-
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(
+                    Intent(this, MainActivity::class.java)
+                )
+                finish()
+            }, 8500)
         }
     }
 }
 
 
-@DelicateCoroutinesApi
-@SuppressLint("CoroutineCreationDuringComposition")
 @ExperimentalAnimationApi
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun LauncherScreenMainView() {
+    AppIconAppearanceAnimate(modifier = Modifier)
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun AppIconAppearanceAnimate(modifier: Modifier) {
     // context
     val context = LocalContext.current
 
@@ -49,32 +66,33 @@ fun LauncherScreenMainView() {
         color = Color.Transparent
     )
 
-    // visible state
-    val fadeInAnim by animateFloatAsState(
+    //infinity transition
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val imageAlpha by infiniteTransition.animateFloat(
+        initialValue = 0f,
         targetValue = 1f,
-        animationSpec = tween(
-            durationMillis = 3000,
+        animationSpec = InfiniteRepeatableSpec(
+            animation = tween(
+                durationMillis = 4000,
+                delayMillis = 100,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Reverse
         )
     )
-
-
-
-
-
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-
+        Image(
+            painter = painterResource(id = R.mipmap.app_icon),
+            contentDescription = "Icon",
+            modifier = Modifier.alpha(imageAlpha)
+        )
     }
 
 }
-
-//Image(
-//                painter = painterResource(id = R.mipmap.app_icon),
-//                contentDescription = ""
-//            )
-
 
 
--- app/src/main/java/com/example/pro1221_android_petshopmanagement/screen/MainActivity.kt
+++ app/src/main/java/com/example/pro1221_android_petshopmanagement/screen/MainActivity.kt
@@ -11,8 +11,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -20,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -28,79 +25,25 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.pro1221_android_petshopmanagement.ui.theme.PRO1221_Android_PetShopManagementTheme
 
 class MainActivity : ComponentActivity() {
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PRO1221_Android_PetShopManagementTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    MainContent()
-                }
-            }
+            MainContent()
         }
     }
 }
-
-@ExperimentalAnimationApi
-@Composable
-fun MainContent(){
-    val longtext = "Lorem Ipsum is simply dummy text of the printing" +
-            " and typesetting industry. Lorem Ipsum has been the" +
-            " industry's standard dummy text ever since the 1500s," +
-            " when an unknown printer took a galley of type and" +
-            " scrambled it to make a type specimen book."
-
-    val isVisible = remember { mutableStateOf(true) }
-
-    Column(
-        modifier = Modifier
-            .background(Color(0xFFEDEAE0))
-            .padding(16.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ){
-        Button(
-            onClick = {
-                isVisible.value = !isVisible.value
-            },
-        ) {
-            Text(if (isVisible.value)"Hide" else "Show")
-        }
-
-        AnimatedVisibility(
-            visible = isVisible.value,
-            enter = fadeIn(),
-            exit = fadeOut()
-        ) {
-            Box(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(Color(0xFFD2691E))
-                    .padding(16.dp)
-                    .fillMaxSize()
-            ) {
-                Text(
-                    text = longtext,
-                    fontSize = 35.sp,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    fontFamily = FontFamily.Cursive,
-                    color = Color(0xFF58111A),
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
-        }
-    }
-}
-
 
 @ExperimentalAnimationApi
 @Preview
 @Composable
 fun ComposablePreview(){
     MainContent()
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun MainContent() {
 }
